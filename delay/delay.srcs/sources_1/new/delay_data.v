36a7cc7ecb17d6bdb2c82484a21caab31c2aebb0
Paleface101 <57947801+Paleface101@users.noreply.github.com>
2021-08-09 17:50:44 +0300

    2

--- delay/delay.srcs/sources_1/new/delay_data.v
+++ delay/delay.srcs/sources_1/new/delay_data.v
@@ -60,33 +60,30 @@ always @( posedge ACLK ) begin
         m_axis_tstrb_int    <= 0;
         s_axis_tready_int   <= 0;
         m_axis_tvalid_int   <= 0;
-       // m_axis_tdata_delay <= 0;//delay
         counter             <= 0;
         m_axis_tvalid_delay <= 0;
         m_axis_tstrb_delay  <= 0;
         m_axis_tkeep_delay  <= 0;
-        
-        
      end else begin
         s_axis_tready_int <= m_axis_tready;
-    if      ( !enable )       
+     if      ( !enable )       
                 counter <= 1'b0;
-    else if ( enable && s_axis_tready && s_axis_tvalid ) 
+     else if ( enable && s_axis_tready && s_axis_tvalid ) 
                 counter <= counter + 1'b1;
                 
-       if ( s_axis_tready && s_axis_tvalid && counter >= delay ) begin
-            //m_axis_tvalid_int <= s_axis_tvalid;
-             m_axis_tvalid_delay <= s_axis_tvalid;
-             m_axis_tkeep_delay  <= s_axis_tkeep;
-             m_axis_tstrb_delay  <= s_axis_tstrb;
-           // m_axis_tdata_int  <= s_axis_tdata;
-          //  m_axis_tdata_delay <= s_axis_tdata;
-            //m_axis_tdata_int <= m_axis_tdata_delay;
-             counter             <= counter;
-       end
+     if ( s_axis_tready && s_axis_tvalid && counter >= delay ) begin
+          //m_axis_tvalid_int <= s_axis_tvalid;
+            m_axis_tvalid_delay <= s_axis_tvalid;
+            m_axis_tkeep_delay  <= s_axis_tkeep;
+            m_axis_tstrb_delay  <= s_axis_tstrb;
+         // m_axis_tdata_int  <= s_axis_tdata;
+        //  m_axis_tdata_delay <= s_axis_tdata;
+          //m_axis_tdata_int <= m_axis_tdata_delay;
+            counter             <= counter;
+     end
            else if( !s_axis_tvalid ) 
-          // m_axis_tvalid_int  <= 0;
-           m_axis_tvalid_delay <= 0;
+         // m_axis_tvalid_int   <= 0;
+            m_axis_tvalid_delay <= 0;
            
      /*  if ( s_axis_tready && s_axis_tvalid ) begin
        m_axis_tkeep_delay <= s_axis_tkeep;
@@ -95,13 +92,12 @@ always @( posedge ACLK ) begin
        m_axis_tstrb_int   <= m_axis_tstrb_delay;
        end  */  
        if (s_axis_tready && m_axis_tvalid_delay && counter >= delay)  begin
-        m_axis_tvalid_int  <= m_axis_tvalid_delay ;
-        m_axis_tdata_int   <= s_axis_tdata;
-        m_axis_tkeep_int   <= m_axis_tkeep_delay;
-        m_axis_tstrb_int   <= m_axis_tstrb_delay;
-    end else m_axis_tvalid_int <= 0; 
-       
-       
+             m_axis_tvalid_int <= m_axis_tvalid_delay;
+             m_axis_tdata_int  <= s_axis_tdata;
+             m_axis_tkeep_int  <= m_axis_tkeep_delay;
+             m_axis_tstrb_int  <= m_axis_tstrb_delay;
+        end else 
+             m_axis_tvalid_int  <= 0; 
     end   
 end 
 
